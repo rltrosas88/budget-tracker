481dafb6cae11986f748a678cb983faacb3499e6
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-19 13:06:10 -0500

    saveRecord

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,3 +27,15 @@ request.onsuccess = function(event) {
     // log error here
     console.log(event.target.errorCode);
   };
+
+  // This function will be executed if we attempt to submit a new budget and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+  
+    // access the object store for `new_budget`
+    const pizzaObjectStore = transaction.objectStore('new_budget');
+  
+    // add record to your store with add method
+    pizzaObjectStore.add(record);
+  }
